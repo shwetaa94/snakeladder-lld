ea42bf5214075f4ab9f19112d11cc0ac2ea5c88f
shweta <shwetasinghh9416@gmail.com>
2025-05-04 06:12:29 +0530

    first commit

---
+++ snakeladder/src/main/java/com/snakeladder/snakeladder/SnakeladderApplication.java
@@ -0,0 +1,14 @@
+package com.snakeladder.snakeladder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SnakeladderApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(SnakeladderApplication.class, args);
+		System.out.println("Hello, Welcome to Snake Ladder Game!⭐⭐⭐⭐⭐");
+	}
+}
---
+++ snakeladder/src/test/java/com/snakeladder/snakeladder/SnakeladderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.snakeladder.snakeladder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SnakeladderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
